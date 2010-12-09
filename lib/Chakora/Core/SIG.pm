dd9eca0c0aa199b65ddedf6dc40971780f63c5d0
Matthew Barksdale <mattwb65@gmail.com>
2010-12-09 20:47:10 +0000

    Hey, SIG(INT|TERM)s dont require sending bold shit to the logchannel. Something simple like this is fine.

--- lib/Chakora/Core/SIG.pm
+++ lib/Chakora/Core/SIG.pm
@@ -7,7 +7,7 @@ use base 'Exporter';
 our @EXPORT_OK = qw(TERM_handler INT_handler);
 
 sub TERM_handler {
-    logchan( "operserv", "\002!!!\002 Caught \002SIGTERM\002; terminating" );
+    logchan( "operserv", "Exiting on SIGTERM." );
     svsflog( "chakora", "Exiting on SIGTERM" );
     if ( module_exists("chanserv/main") ) {
         serv_quit( "chanserv", "Shutting down" );
@@ -19,7 +19,7 @@ sub TERM_handler {
 }
 
 sub INT_handler {
-    logchan( "operserv", "\002!!!\002 Caught \002SIGINT\002; terminating" );
+    logchan( "operserv", "Exiting on SIGINT." );
     svsflog( "chakora", "Exiting on SIGINT" );
     if ( module_exists("chanserv/main") ) {
         serv_quit( "chanserv", "Shutting down" );
